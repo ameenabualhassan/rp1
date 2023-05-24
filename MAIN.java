9956c9deb3bf98906ead2ab65d447b2c29301dcb
ameenofficial <64777745+ameenofficial@users.noreply.github.com>
2023-05-24 20:20:18 +0300

    adding the quotes

---
+++ MAIN.java
@@ -0,0 +1,20 @@
+package rp1;
+
+public class MAIN {
+
+    String[] gitFlashCards = {
+
+            "Git is a free and open-source distributed version control system.",
+            "In Git, branching and merging are easy and efficient.",
+            "Git allows you to track changes and collaborate with others effectively.",
+            "With Git, you can easily revert to previous versions of your code.",
+            "Git makes it simple to work on multiple features or fixes simultaneously.",
+            "The power of Git lies in its ability to handle large projects with speed and efficiency.",
+            "Git provides a seamless way to handle conflicts and merge code changes.",
+            "Using Git, you can explore and switch between different branches effortlessly.",
+            "Git is widely used in the software development industry for its reliability and versatility.",
+            "The Git community is active and constantly evolving the tool to meet developers' needs."
+
+    };
+
+}
